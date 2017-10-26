121e3a5f9f0584e8e04816b17e2eaf1f4e06358a
ni1lloc <acharry@foxmail.com>
2017-10-26 19:28:07 +0800

    2-dit-fft

--- fft1.cpp
+++ fft1.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <complex>
 #include <cmath>
@@ -5,15 +6,16 @@
 #define PI 3.1415926535897932384626433832795028841971
 using namespace std;
 
-void fft(complex<double> x[], complex<double> y[], int p)
+void fft(complex<double> x[],complex<double> y[], int p)
 {
+	cout<<"start fft:"<<p<<endl;
 	if(p==0)
 	{
 		y[0]=x[0];
 		return;
 	}
 	int n = 1<<p;
-	complex<double> wn(cos(2/n*PI),sin(2/n*PI));
+	complex<double> wn(cos(2.0/n*PI),sin(2.0/n*PI));
 	complex<double> *xo = new complex<double>[n/2];
 	complex<double> *xe = new complex<double>[n/2];
 	complex<double> *yo = new complex<double>[n/2];
@@ -22,8 +24,8 @@ void fft(complex<double> x[], complex<double> y[], int p)
 	int i;
 	for(i=0;i<n/2;i++)
 	{
-		xo[i]=x[i*2];
-		xe[i]=x[i*2+1];
+		xe[i]=x[i*2];
+		xo[i]=x[i*2+1];
 	}
 	fft(xo,yo,p-1);
 	fft(xe,ye,p-1);
@@ -31,9 +33,10 @@ void fft(complex<double> x[], complex<double> y[], int p)
 	complex<double> w(1,0);
 	for(i=0;i<n/2;i++)
 	{
-		t=w*ye(i);
-		y[i]=yo[i]+t;
-		y[i+n/2]=yo-t;
+		t=w*yo[i];
+		cout<<t.real()<<endl;
+		y[i]=ye[i]+t;
+		y[i+n/2]=ye[i]-t;
 		w=w*wn;
 	}
 	delete[] yo;
@@ -53,24 +56,34 @@ int retp(int n)
 	return p;
 }
 
+ifstream fin("test.in");
+ofstream fout("test.out");
 int main()
 {
-	int n,i;
+	int n,i,p;
 	int N;
-	cin>>n;
-	N=1<<retp(n);
+	//cin>>n;
+	fin>>n;
+	p=retp(n);
+	N=1<<p;
 	double t;
 	complex<double> *x = new complex<double>[N];
 	complex<double> *y = new complex<double>[N];
+	for(i=0;i<n;i++){
+		x[i]=0;
+		y[i]=0;
+	}
 	for(i=0;i<n;i++)
 	{
-		cin>>t;
+		//cin>>t;
+		fin>>t;
 		x[i]=t;
 	}
 	fft(x,y,p);
 	for(i=0;i<N;i++)
 	{
-		cout<<y[i].real<<"+j"<<y[i].imag<<endl;
+		//cout<<y[i].real<<"+j"<<y[i].imag<<endl;
+		fout<<y[i].real()<<"+j"<<y[i].imag()<<endl;
 	}
 
 	return 0;
